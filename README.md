65a59ee97b6bc0ef894cc97ee7330b645e19da62
Schilion <ceclemente@up.edu.ph>
2025-02-18 13:55:54 +0800

    Added function to validate if the two passwords are the same and added a test case

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/2EnW9dmo)
+
+
+# Name
+- Clemente, Carl Timothy E.
+# Student Number
+- 2021-68301
+# Degree Program
+- BS Computer Science
+# Key Takeaways
+- 
---
+++ clemente_exer3.js
@@ -0,0 +1,22 @@
+//function to validate if two passwords are the same
+function validatePassword(password1, password2){
+    
+    if (password1 !== password2) return false; // returns false if passwords don't match
+    if (password1.length < 8) return false; // returns false if password doesn't meet minimum length
+
+    // flags to check if it has 1 uppercase, 1 lowercase, and 1 number
+    let hasUpper = false, hasLower = false, hasNumber = false;
+
+    // iterate to each character of the passwords to check it
+    for (let char of password1) {
+        if (char >= 'A' && char <= 'Z') hasUpper = true; // check if character is an uppercase
+        if (char >= 'a' && char <= 'z') hasLower = true; // check if character is a lowercase
+        if (char >= '0' && char <= '9') hasNumber = true; // check if character is a number 
+    }
+
+    return hasUpper && hasLower && hasNumber; // Return true only if all conditions are met
+    
+}
+
+
+console.log(validatePassword("Timothy24", "Timothy24"));
